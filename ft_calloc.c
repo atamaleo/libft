d55ceb4409c79f00ae11c7993c5e92cf68323ed3
viniferr <leovcf28@gmail.com>
2023-10-26 17:32:30 -0300

    added calloc and strdup

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: viniferr <viniferr@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 14:51:47 by viniferr          #+#    #+#             */
-/*   Updated: 2023/10/26 16:04:41 by viniferr         ###   ########.fr       */
+/*   Updated: 2023/10/26 16:29:10 by viniferr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,11 @@
 
 void	*ft_calloc(size_t num, size_t size)
 {
+	void *p;
 
+	p = malloc(size * num);
+	if (p == NULL)
+		return (p);
+	ft_bzero(p, size * num);
+	return (p);
 }
--- ft_strdup.c
+++ ft_strdup.c
@@ -1,14 +1,47 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   strdup.c                                           :+:      :+:    :+:   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: viniferr <viniferr@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 16:59:38 by viniferr          #+#    #+#             */
-/*   Updated: 2023/10/23 16:59:38 by viniferr         ###   ########.fr       */
+/*   Updated: 2023/10/26 17:32:11 by viniferr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+char	*ft_strdup(const char *s)
+{
+	char	*s2;
+	int		i;
+
+	s2 = (char *)malloc(ft_strlen(s) + 1);
+	i = 0;
+	if (!s2)
+		return(NULL);
+	while (s[i])
+	{
+		s2[i] = s[i];
+		i++;
+	}
+	s2[i] = '\0';
+	return (s2);
+}
+
+// C program to demonstrate strdup()
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char source[] = "GeeksForGeeks";
+
+    // A copy of source is created dynamically
+    // and pointer to copy is returned.
+    char* target = ft_strdup(source);
+
+    printf("%s", target);
+    return 0;
+}
