d8be73a973806dca32ad3aa101d7c5caf16d437a
viniferr <leovcf28@gmail.com>
2023-10-26 19:12:17 -0300

    added _fd functions

--- libft.h
+++ libft.h
@@ -29,8 +29,7 @@ char	*ft_strnstr(const char *big, const char *little, size_t len);
 int		ft_atoi(const char *s);
 void	*ft_calloc(size_t num, size_t size);
 char	*ft_strdup(const char *s);
-/*
-PT2
+//
 char	*ft_substr(char const *s, unsigned int start,size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
@@ -38,12 +37,13 @@ char 	**ft_split(char const *s, char c);
 char 	*ft_itoa(int n);
 char 	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void 	ft_striteri(char *s, void (*f)(unsigned int, char*));
+//
 void 	ft_putchar_fd(char c, int fd);
 void 	ft_putstr_fd(char *s, int fd);
 void 	ft_putendl_fd(char *s, int fd);
 void 	ft_putnbr_fd(int n, int fd);
 
-BONUS
+/*BONUS
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
